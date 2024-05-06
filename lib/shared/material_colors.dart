46c1469f10bea104290e2d28cde8e690e47e516e
eriton.cabral <eriton.cabral@nexello.com.br>
2024-05-06 15:54:23 -0300

    Ajustes de cores

--- lib/shared/material_colors.dart
+++ lib/shared/material_colors.dart
@@ -4,10 +4,16 @@ class AppMaterialColor {
   static const MaterialColor valorantAppColors = MaterialColor(
     _primaryColor,
     <int, Color>{
+      50: Color(0xFFFFFFFF),
       100: Color(0xFFFFFFFF),
-      300: Color(0xFF292929),
+      200: Color(0xff6f757b),
+      300: Color(0xff3f474f),
+      400: Color(0xff273039),
       500: Color(_primaryColor),
+      600: Color(0xff0d161f),
+      700: Color(0xff0c141c),
       800: Color(0xFF111111),
+      900: Color(0xFF000000)
     },
   );
 
